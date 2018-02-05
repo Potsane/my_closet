2b499e73436f672fe7d68ab1d599c848cacf007a
Potsane Mohale <PMohale@jhb.dvt.co.za>
2018-02-05 22:42:42 +0200

    adds the database class

---
+++ app/src/main/java/com/example/pmohale/mycloset/database/ClosetDatabase.java
@@ -0,0 +1,41 @@
+package com.example.pmohale.mycloset.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.pmohale.mycloset.database.internal.OutfitDao;
+import com.example.pmohale.mycloset.database.internal.WardrobeItemDao;
+import com.example.pmohale.mycloset.database.internal.WeatherDao;
+import com.example.pmohale.mycloset.entity.WeatherObject;
+
+/**
+ * Created by PMohale on 2018/02/05.
+ */
+
+@Database(entities = {WeatherObject.class}, version = 1)
+public abstract class ClosetDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "closet";
+
+    private static final Object LOCK = new Object();
+
+    private static ClosetDatabase instance;
+
+    //alt -  can make the method synchronized ie public static synchronized
+    public static ClosetDatabase getInstance(Context context) {
+        if (instance == null) {
+            synchronized (LOCK) {
+                instance = Room.databaseBuilder(context.getApplicationContext(), ClosetDatabase.class, ClosetDatabase.DATABASE_NAME).build();
+            }
+        }
+        return instance;
+    }
+
+    public abstract OutfitDao outfitDao();
+
+    public abstract WardrobeItemDao wardrobeItemDao();
+
+    public abstract WeatherDao weatherDao();
+}
