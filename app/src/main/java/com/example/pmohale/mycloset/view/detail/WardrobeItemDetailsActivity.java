055e4e970af8a2986bc036992e51377f39ab3f99
Potsane Mohale <PMohale@jhb.dvt.co.za>
2018-02-11 17:59:31 +0200

    fixes the landing screen

--- app/src/main/java/com/example/pmohale/mycloset/view/detail/WardrobeItemDetailsActivity.java
+++ app/src/main/java/com/example/pmohale/mycloset/view/detail/WardrobeItemDetailsActivity.java
@@ -1,9 +1,13 @@
 package com.example.pmohale.mycloset.view.detail;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.pmohale.mycloset.R;
+import com.example.pmohale.mycloset.view.list.WardrobeItemsListActivity;
 
 public class WardrobeItemDetailsActivity extends AppCompatActivity {
 
@@ -11,5 +15,21 @@ public class WardrobeItemDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wardrobe_item_details);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, WardrobeItemsListActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
